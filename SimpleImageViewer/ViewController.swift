45fe0abae0c140493f7a7cc21baf930aa3a7029e
Karan Gajjar <karangajjar.lp@gmail.com>
2020-11-02 14:37:19 -0500

    Initial Commit

---
+++ SimpleImageViewer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SimpleImageViewer
+//
+//  Created by Karan Gajjar on 11/2/20.
+//  Copyright © 2020 Karan Gajjar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
